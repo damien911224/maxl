ed7f96eb1a7fea12c8b43dc52f3fb498fc328fe5
damien911224 <damien911224@gmail.com>
2021-05-18 14:59:31 +0900

    Modified

--- model_resnet_maxl.py
+++ model_resnet_maxl.py
@@ -271,6 +271,10 @@ cifar100_train_set = torchvision.datasets.CIFAR100(root="/mnt/hdd0",
                                                    train=True, transform=trans_train, download=True)
 cifar100_test_set = torchvision.datasets.CIFAR100(root="/mnt/hdd0",
                                                    train=False, transform=trans_test, download=True)
+cifar100_train_set = torchvision.datasets.CIFAR10(root="/mnt/hdd0",
+                                                   train=True, transform=trans_train, download=True)
+cifar100_test_set = torchvision.datasets.CIFAR10(root="/mnt/hdd0",
+                                                   train=False, transform=trans_test, download=True)
 batch_size = 512
 kwargs = {'num_workers': 20, 'pin_memory': True}
 cifar100_train_loader = torch.utils.data.DataLoader(
@@ -286,7 +290,8 @@ cifar100_test_loader = torch.utils.data.DataLoader(
 # define label-generation model,
 # and optimiser with learning rate 1e-3, drop half for every 50 epochs, weight_decay=5e-4,
 # psi = [5]*20  # for each primary class split into 5 auxiliary classes, with total 100 auxiliary classes
-psi = [5] * 100
+# psi = [5] * 100
+psi = [5] * 10
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 LabelGenerator = LabelGenerator(psi=psi)
 # LabelGenerator = nn.DataParallel(LabelGenerator)
